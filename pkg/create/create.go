ac25ac336fe742750e313cfa14eb1970768f2a28
edwinwalela <edwinwalela@gmail.com>
2023-01-31 15:13:24 +0300

    create file

--- pkg/create/create.go
+++ pkg/create/create.go
@@ -6,13 +6,15 @@ import (
 )
 
 func CreateSite() {
-	if err := os.Mkdir("site", os.ModePerm); err != nil {
-		log.Fatal(err)
+	if _, err := os.Stat("site"); os.IsNotExist(err) {
+		if err := os.Mkdir("site", os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	about := []byte("# About\n")
 
-	if err := os.WriteFile("/site/about.md", about, 0644); err != nil {
+	if err := os.WriteFile("site/about.md", about, 0644); err != nil {
 		log.Fatal(err)
 	}
 
