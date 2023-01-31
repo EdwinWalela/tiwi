ea043289e7195023f29fc4243e977cc2d2449c87
edwinwalela <edwinwalela@gmail.com>
2023-01-31 15:15:10 +0300

    deconstruct create command

--- pkg/create/create.go
+++ pkg/create/create.go
@@ -5,17 +5,26 @@ import (
 	"os"
 )
 
-func CreateSite() {
+func createParentFolder() {
 	if _, err := os.Stat("site"); os.IsNotExist(err) {
 		if err := os.Mkdir("site", os.ModePerm); err != nil {
 			log.Fatal(err)
 		}
 	}
+}
 
+func createPages() {
+	createPage()
+}
+
+func createPage() {
 	about := []byte("# About\n")
 
 	if err := os.WriteFile("site/about.md", about, 0644); err != nil {
 		log.Fatal(err)
 	}
+}
 
+func CreateSite() {
+	createParentFolder()
 }
