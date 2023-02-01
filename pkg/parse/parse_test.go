dd5c17339429c0efe9f371e6fe71dcc85d67de7f
edwinwalela <edwinwalela@gmail.com>
2023-02-01 22:53:09 +0300

    test create output folder

--- pkg/parse/parse_test.go
+++ pkg/parse/parse_test.go
@@ -10,8 +10,27 @@ import (
 func TestGetPages(t *testing.T) {
 	pages, err := getPages("../../test-site")
 	assert.NoError(t, err)
-
 	assert.Equal(t, 2, len(pages))
+}
+
+func TestCreateOutputFolder(t *testing.T) {
+	directory := "../../test-site"
+	err := createOutputFolder(directory)
+
+	assert.NoError(t, err)
+	assert.DirExists(t, directory+"/static")
+}
+
+func TestReadMarkDown(t *testing.T) {
+
+}
+
+func TestParseMd(t *testing.T) {
+
+}
+
+func TestWriteHTML(t *testing.T) {
+
 }
 
 func TestBuildProject(t *testing.T) {
