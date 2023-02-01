4931963325c7f5ff68ebdea20feed352de32e609
edwinwalela <edwinwalela@gmail.com>
2023-02-01 22:44:07 +0300

    test get markdown pages function

--- pkg/parse/parse_test.go
+++ pkg/parse/parse_test.go
@@ -7,6 +7,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGetPages(t *testing.T) {
+	pages, err := getPages("../../test-site")
+	assert.NoError(t, err)
+
+	assert.Equal(t, 2, len(pages))
+}
+
 func TestBuildProject(t *testing.T) {
 	args := []string{"../../test-site"}
 	Build(args)
