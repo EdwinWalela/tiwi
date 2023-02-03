7153459288c73679237b415edfb19cf3ed8f7917
edwinwalela <edwinwalela@gmail.com>
2023-02-03 22:37:16 +0300

    update watch command description

--- cmd/tiwi/watch.go
+++ cmd/tiwi/watch.go
@@ -7,7 +7,7 @@ import (
 
 var watchCmd = &cobra.Command{
 	Use:   "watch",
-	Short: "Live reload HTML files",
+	Short: "Build and live reload HTML",
 	Long:  "Watches markdown files and rebuilds HTML on file change",
 	Run: func(cmd *cobra.Command, args []string) {
 		watch.Watch(args)
